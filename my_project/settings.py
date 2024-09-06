e4fd8401be5aa25b22f6a78f2f452090ff1d4e28
Helen Proudman <helen.proudman@students.codeinstitute.net>
2024-09-06 13:44:54 +0000

    testing debugging

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x7ais^3)r5=3nllf)6r7%eq^h25fg^7g$dr2x-_dylujl#n339'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-helproudman-djangoproje-abpo5cdhg1b.ws.codeinstitute-ide.net', '.herokuapp.com']
 
